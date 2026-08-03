bcaeac18b9fec31f52ee6c290e39c6d1a39f08b0
rezaul3950 <mdrezaulkarim4119@gmail.com>
2026-08-03 22:02:38 +0600

    Remove Cart button from mobile bottom navigation

--- src/components/layout/MobileBottomNav.jsx
+++ src/components/layout/MobileBottomNav.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router-dom'
-import { FiHome, FiGrid, FiShoppingCart, FiUser } from 'react-icons/fi'
+import { FiHome, FiGrid, FiUser } from 'react-icons/fi'
 
 function MobileBottomNav() {
   const location = useLocation()
@@ -7,7 +7,6 @@ function MobileBottomNav() {
   const navItems = [
     { path: '/', icon: FiHome, label: 'Home' },
     { path: '/products', icon: FiGrid, label: 'Categories' },
-    { path: '/cart', icon: FiShoppingCart, label: 'Cart' },
     { path: '/profile', icon: FiUser, label: 'Profile' }
   ]
 
